6230271fd7d0f148a5f2b7cc0f5f5ddbe5c9073c
Apoorv Joshi <apoorvjoshi@Apoorvs-MacBook-Air.local>
2022-04-21 09:16:29 +0530

    whereClause done.

--- controller/productController.js
+++ controller/productController.js
@@ -2,6 +2,7 @@ const product = require('../models/product');
 const bigPromise = require('../middleware/bigPromise');
 const cloudinary = require('cloudinary');
 const CustomError = require('../utils/customError');
+const WhereClause = require('../utils/whereClause');
 
 exports.addProduct = bigPromise(async(req , res , next)=>{
 
@@ -33,4 +34,25 @@ exports.addProduct = bigPromise(async(req , res , next)=>{
         product
     })
 
+})
+
+exports.getAllProduct = bigPromise(async(req,res,next)=>{
+    const resultPerPage = 6;//lets say by default we want to display 6 result only.
+    const totalProductCount = await Product.countDocuments();
+
+    const products = new WhereClause(Product.find() , req.query).search().filter();
+
+    const filteredProductNumber = products.length;
+
+    products.pager(resultPerPage)
+
+    products = await products.base;
+
+    res.status(200).json({
+        success:true,
+        message:"All products extracted successfully..",
+        products,
+        filteredProductNumber,
+        totalProductCount
+    })
 })
--- utils/whereClause.js
+++ utils/whereClause.js
@@ -17,4 +17,38 @@ class WhereClause{
         this.base = this.base.find({...searchWord}); // this is similar to db.find(email:{"aj.aj.com"})
         return this
     }
+   
+    filter(){
+        let copyQ = {...this.bigQ};
+
+        //remove unwanted field (deletion from object)
+        delete copyQ["search"];
+        delete copyQ["limit"];
+        delete copyQ["page"];
+
+        //convetiong our query object to string such that we can apply replace methods
+        let stringOfCopyQ = JSON.stringify(copyQ);
+
+        stringOfCopyQ = stringOfCopyQ.replace(/\b(gte|lte|gt|lt)\b/g , m=>`$${m}`)
+
+        // converting back to object
+
+        const jsonOfCopyQ = JSON.parse(stringOfCopyQ);
+
+        this.base = this.base.find(jsonOfCopyQ);
+    }
+    pager(resultPerPage){
+        let currPage = 1; // by-default
+        if(this.bigQ.page){
+            currPage = this.bigQ.page;
+        }
+
+        let skipVal = resultPerPage * (currPage-1); // if currPage = 2 && resultPerPage = 5 than we will skip = {5 * (2-1) = 5 initial product} similarly if we are in the 1st page obiously we will print 1st 5 product no skip {5*(1-1) = 0} 
+
+        this.base = this.base.limit(resultPerPage).skip(skipVal);
+
+        return this;
+    }
 }
+
+module.exports = WhereClause;
